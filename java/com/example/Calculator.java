fb4dfef56f6d25e0891c7b606613e09036677852
Padmanabhan17 <padmanabhanmsabari17@gmail.com>
2020-08-06 19:11:59 +0530

    Add files via upload

---
+++ java/com/example/Calculator.java
@@ -0,0 +1,24 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Calculator {
+
+  public double Add (double num1, double num2) {
+	  return num1 + num2;  
+  }
+  
+  public double Sub (double num1, double num2) {
+	  return num1 - num2;  
+  }
+  
+  public double Mul (double num1, double num2) {
+	  return num1 * num2;  
+  }
+  
+  public double Div (double num1, double num2) {
+	  return num1 / num2;  
+  }
+	
+}
